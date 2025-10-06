6de0b2faf8deec605e07f56cb365f7f793211adc
Charles Hudson <charles.hudson@contentful.com>
2025-10-06 15:15:41 +0200

    🐞 fix(e2e): Fixing issues with .env requirement
    NoClose
    
    [[NT-1588](https://contentful.atlassian.net/browse/NT-1588)]

--- implementations/node/src/app.ts
+++ implementations/node/src/app.ts
@@ -1,8 +1,14 @@
 import Optimization from '@contentful/optimization-node'
 import express, { type Express } from 'express'
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+  windowMs: 900_000,
+  max: 5,
+})
 
 const app: Express = express()
-const port = 3000
+app.use(limiter)
 
 const sdk = new Optimization({
   optimizationKey: process.env.VITE_NINETAILED_CLIENT_ID ?? '',
@@ -12,14 +18,16 @@ const sdk = new Optimization({
   contentSpaceId: process.env.VITE_CONTENTFUL_SPACE_ID ?? '',
   logLevel: 'debug',
   api: {
-    analytics: { baseUrl: 'http://localhost/insights/' },
-    personalization: { baseUrl: 'http://localhost/experience/' },
+    analytics: { baseUrl: process.env.VITE_INSIGHTS_API_BASE_URL },
+    personalization: { baseUrl: process.env.VITE_EXPERIENCE_API_BASE_URL },
   },
 })
 
 app.get('/', (_req, res) => {
   res.send(sdk.config.optimizationKey)
 })
+
+const port = 3000
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console -- debug
