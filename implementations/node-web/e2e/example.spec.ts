8013c94268eb6c8be18b371f9d2477d935ce7a81
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-10-24 14:38:18 +0200

    update

--- implementations/node-web/e2e/example.spec.ts
+++ implementations/node-web/e2e/example.spec.ts
@@ -2,13 +2,13 @@ import { expect, test } from '@playwright/test'
 
 const CLIENT_ID = process.env.VITE_NINETAILED_CLIENT_ID ?? 'error'
 
-test('responds with the client ID', async ({ request }) => {
-  const response = await request.get('/')
+test('test SSR', async ({ request }) => {
+  const response = await request.get('http://localhost:3000/')
 
   expect(await response.text()).toEqual(CLIENT_ID)
 })
 
-test('displays client ID', async ({ page }) => {
+test('test CSR', async ({ page }) => {
   await page.goto('http://localhost:4000/')
 
   await expect(page.getByTestId('clientId')).toHaveText(CLIENT_ID)
