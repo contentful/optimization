95669b9662b77227d99853e360bae124108be061
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-10-30 12:07:30 +0100

    remove css url

--- implementations/node-ssr/e2e/example.spec.ts
+++ implementations/node-ssr/e2e/example.spec.ts
@@ -2,22 +2,17 @@ import { ANONYMOUS_ID_COOKIE } from '@contentful/optimization-core'
 import { expect, test } from '@playwright/test'
 
 const CLIENT_ID = process.env.VITE_NINETAILED_CLIENT_ID ?? 'error'
-
 const ANONYMOUS_ID = '__ctfl_opt_anonymous_id__'
-
-const URI = {
-  ssr: 'http://localhost:3000/',
-  csr: 'http://localhost:4000/',
-}
+const URI = 'http://localhost:3000/'
 
 test('SSR/ check client ID', async ({ request }) => {
-  const response = await request.get(URI.ssr)
+  const response = await request.get(URI)
 
   expect(await response.text()).toContain(`"clientId":"${CLIENT_ID}"`)
 })
 
 test('SSR/ set Profile id from backend', async ({ context, page }) => {
-  await page.goto(URI.ssr)
+  await page.goto(URI)
 
   const state = await context.storageState()
 
@@ -29,7 +24,7 @@ test('SSR/ refresh profile id from backend', async ({ context, page }) => {
     { name: ANONYMOUS_ID_COOKIE, value: 'old-profile-id', path: '/', domain: 'localhost' },
   ])
 
-  await page.goto(URI.ssr)
+  await page.goto(URI)
 
   const state = await context.storageState()
 
