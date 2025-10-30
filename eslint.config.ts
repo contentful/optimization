91f734f8d91d84cc797b4c0c9cd80cfc64b8de2b
Alex Freas <akfreas@users.noreply.github.com>
2025-10-30 15:49:54 +0100

    [NT-1752] Remove TestTrackingScreen ignore from eslint.config

--- eslint.config.ts
+++ eslint.config.ts
@@ -14,7 +14,6 @@ const config: ConfigArray = typescript.config(
       '**/ios/**',
       '**/build/**',
       '**/.bundle/**',
-      '**/TestTrackingScreen.tsx',
     ],
   },
   js.configs.recommended,
