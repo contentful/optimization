aac05c14d680d3215a05d7b1bcdb13e7d29e9bda
Charles Hudson <charles.hudson@contentful.com>
2025-10-31 12:48:18 +0100

    🐞 fix(node): Fixing forgotten update in the Node dashboard
    Also fixing outdated command names for starting the mock server
    
    [[NT-1872](https://contentful.atlassian.net/browse/NT-1872)]

--- implementations/node/playwright.config.mjs
+++ implementations/node/playwright.config.mjs
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'pnpm --filter e2e mocks:serve',
+  //   command: 'pnpm --filter e2e serve',
   //   url: 'http://localhost',
   //   reuseExistingServer: !isCI,
   // },
