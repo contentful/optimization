2991eb8ba2bc69790e3d4ff745aa5b7a584538bb
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2025-10-31 16:12:33 +0100

    update

--- implementations/node-ssr/e2e/example.spec.ts
+++ implementations/node-ssr/e2e/example.spec.ts
@@ -7,7 +7,16 @@ const URI = 'http://localhost:3000/'
 
 const id = '89a085900309de9ecef46965112c309d6f00f1aaed7fcb6709eb851c9557ec42'
 
-test('check client ID rendered from Optimization API on server-side render', async ({
+function genAnonymousIdCookie(id: string): {
+  name: string
+  value: string
+  path: string
+  domain: string
+} {
+  return { name: ANONYMOUS_ID_COOKIE, value: id, path: '/', domain: 'localhost' }
+}
+
+test('BACKEND: check client ID rendered from Optimization API on server-side render', async ({
   request,
 }) => {
   const response = await request.get(URI)
@@ -15,30 +24,38 @@ test('check client ID rendered from Optimization API on server-side render', asy
   expect(await response.text()).toContain(`"clientId":"${CLIENT_ID}"`)
 })
 
-test('check client ID rendered from Optimization API on client-side render', async ({ page }) => {
+test('BACKEND: generates new Profile id', async ({ context, page }) => {
+  await page.goto(URI)
+
+  const state = await context.storageState()
+
+  expect(state.origins[0]?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: id }])
+})
+
+test('BACKEND: identifies profile id from client', async ({ context, page }) => {
+  // const id = 'custom-profile-id'  TODO: fix me
+  await context.addCookies([genAnonymousIdCookie(id)])
+  await page.goto(URI)
+
+  const state = await context.storageState()
+
+  expect(state.origins[0]?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: id }])
+})
+
+test("BACKEND: can't identify profile id from client", async ({ context, page }) => {
+  // const id = 'custom-profile-id'  TODO: fix me
+  await context.addCookies([genAnonymousIdCookie(id)])
+  await page.goto(URI)
+
+  const state = await context.storageState()
+
+  expect(state.origins[0]?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: id }])
+})
+
+test('FRONTEND: check client ID rendered from Optimization API on client-side render', async ({
+  page,
+}) => {
   await page.goto('/')
 
   await expect(page.getByTestId('clientId')).toHaveText(CLIENT_ID)
-})
-
-test('set Profile id from backend', async ({ context, page }) => {
-  await page.goto(URI)
-
-  const state = await context.storageState()
-
-  expect(state.origins[0]?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: id }])
-})
-
-test('backend uses profile id from client', async ({ context, page }) => {
-  // const id = 'custom-profile-id'  TODO: fix me
-
-  await context.addCookies([
-    { name: ANONYMOUS_ID_COOKIE, value: id, path: '/', domain: 'localhost' },
-  ])
-
-  await page.goto(URI)
-
-  const state = await context.storageState()
-
-  expect(state.origins[0]?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: id }])
 })
