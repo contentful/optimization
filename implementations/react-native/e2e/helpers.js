a4511889aba5fbcd522fe9ad116c47d10f54bdd9
Alex Freas <akfreas@users.noreply.github.com>
2025-12-02 14:53:52 +0100

    [NT-1910] Remove duplicate react-native-dotenv in package, don't distinguish platform when clearing profile state

--- implementations/react-native/e2e/helpers.js
+++ implementations/react-native/e2e/helpers.js
@@ -1,12 +1,8 @@
 async function clearProfileState() {
   const platform = device.getPlatform()
 
-  if (platform === 'android') {
-    // await device.resetApp()
-  } else {
-    await device.terminateApp()
-    await device.launchApp({ delete: true })
-  }
+  await device.terminateApp()
+  await device.launchApp({ delete: true })
 }
 
 module.exports = {
