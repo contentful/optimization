ae7db7080bedc95a4bc4ec2ca1e65a1804f43508
Alex Freas <akfreas@users.noreply.github.com>
2025-12-02 14:58:55 +0100

    [NT-1910] Fix type declarations in react native implementation

--- implementations/react-native/types/env.d.ts
+++ implementations/react-native/types/env.d.ts
@@ -1,11 +1,11 @@
 declare module '@env' {
-  export const CONTENTFUL_SPACE_ID: string
-  export const CONTENTFUL_ENVIRONMENT: string
-  export const CONTENTFUL_TOKEN: string
-  export const CONTENTFUL_CDA_HOST: string
-  export const CONTENTFUL_BASE_PATH: string
-  export const NINETAILED_CLIENT_ID: string
-  export const NINETAILED_ENVIRONMENT: string
-  export const EXPERIENCE_API_BASE_URL: string
-  export const INSIGHTS_API_BASE_URL: string
+  export const VITE_CONTENTFUL_SPACE_ID: string
+  export const VITE_CONTENTFUL_ENVIRONMENT: string
+  export const VITE_CONTENTFUL_TOKEN: string
+  export const VITE_CONTENTFUL_CDA_HOST: string
+  export const VITE_CONTENTFUL_BASE_PATH: string
+  export const VITE_NINETAILED_CLIENT_ID: string
+  export const VITE_NINETAILED_ENVIRONMENT: string
+  export const VITE_EXPERIENCE_API_BASE_URL: string
+  export const VITE_INSIGHTS_API_BASE_URL: string
 }
