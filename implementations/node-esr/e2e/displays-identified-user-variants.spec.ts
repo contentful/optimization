522df0091ae90aba581485ea6715b0f73d5976e6
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-05 14:53:23 +0100

    basi setup for e2e tests

--- implementations/node-esr/e2e/displays-identified-user-variants.spec.ts
+++ implementations/node-esr/e2e/displays-identified-user-variants.spec.ts
@@ -1,61 +1,39 @@
 import { ANONYMOUS_ID_COOKIE } from '@contentful/optimization-core'
-import { expect, test } from '@playwright/test'
+import { type Response, expect, test } from '@playwright/test'
 
 const CLIENT_ID = process.env.VITE_NINETAILED_CLIENT_ID ?? 'error'
 const ANONYMOUS_ID = '__ctfl_opt_anonymous_id__'
 
-const UID_LENGTH = 36
-
-function genAnonymousIdCookie(id: string): {
-  name: string
-  value: string
-  path: string
-  domain: string
-} {
-  return { name: ANONYMOUS_ID_COOKIE, value: id, path: '/', domain: 'localhost' }
-}
-
 test.describe('identified user', () => {
   let customIdentifiedId: string
+  let response: Response | null = null
 
   test.beforeEach(async ({ page, context }) => {
     customIdentifiedId = 'custom-profile-id'
-    await context.addCookies([genAnonymousIdCookie(customIdentifiedId)])
-    await page.goto(`/user/maximus`)
-    // await page.goto('/?userId=someone')
+    const cookie = {
+      name: ANONYMOUS_ID_COOKIE,
+      value: customIdentifiedId,
+      path: '/',
+      domain: 'localhost',
+    }
+
+    await context.addCookies([cookie])
+    response = await page.goto(`/user/someone`)
     await page.waitForLoadState('domcontentloaded')
   })
 
-  test('BACKEND: check client ID rendered from Optimization API on server-side render', async ({
-    request,
-  }) => {
-    const response = await request.get('/')
-
-    expect(await response.text()).toContain(`"clientId":"${CLIENT_ID}"`)
+  test('check client ID rendered from Optimization API on server-side render', async () => {
+    expect(await response?.text()).toContain(`"clientId":"${CLIENT_ID}"`)
   })
 
-  test('BACKEND: generates new Profile id', async ({ context, page }) => {
-    await page.goto('/')
-
-    const state = await context.storageState()
-
-    const storage = state.origins[0]?.localStorage ?? []
-    const storedId = storage.find((item) => item.name === ANONYMOUS_ID)?.value
-
-    expect(storedId).toBeDefined()
-    expect(storedId).toHaveLength(UID_LENGTH)
-  })
-
-  test('BACKEND: identifies profile id and associates it with user id', async ({ context }) => {
-    const { origins: [origin] } = await context.storageState()
-    expect(origin?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: customIdentifiedId }])
-  })
-
-  test('FRONTEND: check client ID rendered from Optimization API on client-side render', async ({
-    page,
-  }) => {
-    await page.goto('/')
-
+  test('check client ID rendered from Optimization API on client-side render', async ({ page }) => {
     await expect(page.getByTestId('clientId')).toHaveText(CLIENT_ID)
+  })
+
+  test('identifies profile id and associates it with user id', async ({ context }) => {
+    const {
+      origins: [origin],
+    } = await context.storageState()
+    expect(origin?.localStorage).toEqual([{ name: ANONYMOUS_ID, value: customIdentifiedId }])
   })
 })
--- implementations/node-esr/e2e/displays-unidentified-user-variants.spec.ts
+++ implementations/node-esr/e2e/displays-unidentified-user-variants.spec.ts
@@ -1,24 +1,27 @@
-import { expect, test } from '@playwright/test'
+import { type Response, expect, test } from '@playwright/test'
 
 const CLIENT_ID = process.env.VITE_NINETAILED_CLIENT_ID ?? 'error'
 const ANONYMOUS_ID = '__ctfl_opt_anonymous_id__'
 const UID_LENGTH = 36
 
 test.describe('unidentified user', () => {
+  let response: Response | null = null
+
   test.beforeEach(async ({ page }) => {
-    await page.goto('/')
-    // await page.waitForLoadState('domcontentloaded')
+    response = await page.goto('/')
+    await page.waitForLoadState('domcontentloaded')
   })
 
-  test('BACKEND: check client ID rendered from Optimization API on server-side render', async ({
-    request,
-  }) => {
-    const response = await request.get('/')
-
-    expect(await response.text()).toContain(`"clientId":"${CLIENT_ID}"`)
+  test('check client ID rendered from Optimization API on server-side render', async () => {
+    expect(await response?.text()).toContain(`"clientId":"${CLIENT_ID}"`)
   })
 
-  test('BACKEND: generates new Profile id', async ({ context, page }) => {
+  test('check client ID rendered from Optimization API on client-side render', async ({ page }) => {
+    await expect(page.getByTestId('clientId')).toHaveText(CLIENT_ID)
+  })
+
+
+  test('generates new Profile id', async ({ context, page }) => {
     await page.goto('/')
 
     const state = await context.storageState()
@@ -28,14 +31,5 @@ test.describe('unidentified user', () => {
 
     expect(storedId).toBeDefined()
     expect(storedId).toHaveLength(UID_LENGTH)
-  })
-
-
-  test('FRONTEND: check client ID rendered from Optimization API on client-side render', async ({
-    page,
-  }) => {
-    await page.goto('/')
-
-    await expect(page.getByTestId('clientId')).toHaveText(CLIENT_ID)
   })
 })
