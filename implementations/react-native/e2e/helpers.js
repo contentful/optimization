8543d6291b4f5c650361ec968f039b041a1d18b8
Alex Freas <akfreas@users.noreply.github.com>
2026-01-07 16:55:38 +0100

    Element visibility timeout set to 10000 as on main

--- implementations/react-native/e2e/helpers.js
+++ implementations/react-native/e2e/helpers.js
@@ -1,5 +1,5 @@
 // Timeout value for waiting for elements to become visible (in milliseconds)
-const ELEMENT_VISIBILITY_TIMEOUT = 120000 // 120 seconds
+const ELEMENT_VISIBILITY_TIMEOUT = 10000 // 120 seconds
 
 async function clearProfileState() {
   const platform = device.getPlatform()
