a267d625a0ee47487de2eabcf509340b685db6cb
Alex Freas <akfreas@users.noreply.github.com>
2026-01-26 12:05:16 +0100

    Hide log box totally for implemntation app

--- implementations/react-native/index.js
+++ implementations/react-native/index.js
@@ -6,16 +6,6 @@ import { AppRegistry, LogBox } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-// Disable LogBox warnings/errors for E2E tests to prevent toast overlays
-// Check if we're running in a Detox test environment
-// Detox sets certain globals and launchArgs that we can detect
-const isDetoxTest =
-  typeof global.__DETOX_INITIALIZED__ !== 'undefined' ||
-  typeof global.__DETOX__ !== 'undefined' ||
-  (typeof global !== 'undefined' && global.detox !== undefined)
-
-if (isDetoxTest) {
-  LogBox.ignoreAllLogs()
-}
+LogBox.ignoreAllLogs()
 
 AppRegistry.registerComponent(appName, () => App)
