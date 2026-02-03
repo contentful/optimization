563891f3bd7d4106d7cc37ecf9ab123363cb8de9
Charles Hudson <265039+phobetron@users.noreply.github.com>
2026-02-03 17:43:53 +0100

    🔥 feat(legacy): Adding migration logic from legacy experience.js (#131)

--- universal/core/src/global-constants.ts
+++ universal/core/src/global-constants.ts
@@ -20,3 +20,59 @@ export const OPTIMIZATION_CORE_SDK_VERSION =
  * ```
  */
 export const ANONYMOUS_ID_COOKIE = 'ctfl-opt-aid'
+
+/**
+ * Storage key for the anonymous identifier.
+ *
+ * @internal
+ */
+export const ANONYMOUS_ID_KEY = '__ctfl_opt_anonymous_id__'
+
+/**
+ * Storage key for the persisted consent status.
+ *
+ * @internal
+ */
+export const CONSENT_KEY = '__ctfl_opt_consent__'
+
+/**
+ * Storage key for cached Custom Flags.
+ *
+ * @internal
+ */
+export const CHANGES_CACHE_KEY = '__ctfl_opt_changes__'
+
+/**
+ * Storage key for the debug flag toggle.
+ *
+ * @internal
+ */
+export const DEBUG_FLAG_KEY = '__ctfl_opt_debug__'
+
+/**
+ * Storage key for cached profile data.
+ *
+ * @internal
+ */
+export const PROFILE_CACHE_KEY = '__ctfl_opt_profile__'
+
+/**
+ * Storage key for cached selected personalizations.
+ *
+ * @internal
+ */
+export const PERSONALIZATIONS_CACHE_KEY = '__ctfl_opt_personalizations__'
+
+/**
+ * Legacy anoynmous ID cookie key for migration from experience.js
+ *
+ * @internal
+ */
+export const ANONYMOUS_ID_COOKIE_LEGACY = 'ntaid'
+
+/**
+ * Legacy anoynmous ID storage key for migration from experience.js
+ *
+ * @internal
+ */
+export const ANONYMOUS_ID_KEY_LEGACY = '__nt_anonymous_id__'
